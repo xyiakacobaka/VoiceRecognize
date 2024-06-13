57fdff623a2c94405c79bc834b88378ca19b5d15
xyiakacobaka <theneetfreekgames@gmail.com>
2024-06-13 19:54:56 +0300

    Добавьте файлы проекта.

---
+++ VoiceRecognize/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Vosk;
+
+namespace VoiceRecognize
+{
+    internal class Program
+    {
+        public static void DemoBytes(Model model)
+        {
+            VoskRecognizer rec = new VoskRecognizer(model, 16000.0f);
+            rec.SetMaxAlternatives(0);
+            rec.SetWords(true);
+            using (Stream source = File.OpenRead("decoder-test.wav"))//файл должен быть с такми зарактеристиками: 16khz 16bit mono Wav
+
+            {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    if (rec.AcceptWaveform(buffer, bytesRead))
+                    {
+                        Console.WriteLine(rec.Result());
+                    }
+                    else
+                    {
+                        Console.WriteLine(rec.PartialResult());
+                    }
+                }
+            }
+            Console.WriteLine(rec.FinalResult());
+        }
+        static void Main(string[] args)
+        {
+            Vosk.Vosk.SetLogLevel(-1);
+            Model model = new Model("C:\\Users\\goga\\Desktop\\Шняга\\VoiceRecognize\\VoiceRecognize\\vosk-model-small-ru-0.22");//Модель языка
+            DemoBytes(model);
+        }
+    }
+}
